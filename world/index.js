5686e7884441812a8152b1f2e91c18d1bc801517
Alexandra White <alexandra.n.white@gmail.com>
2016-09-02 11:01:59 -0400

    Add World (#3)
    * Add "World"
    
    Updated docker-compose to add “world”; added World to front-end
    container pilot and function to index.html; added world folder
    
    * add hello to nginx conf compel

---
+++ world/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Load modules
+
+const Http = require('http');
+
+
+const server = module.exports = Http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('World');
+});
+
+server.listen(3002, () => {
+  console.log(`World server listening on port ${server.address().port}`);
+});
